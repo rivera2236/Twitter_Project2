a5c2ba25c643da4f4e4c684f28389ab3f10f8efe
jorge rivera <rivera2236@gmail.com>
2019-10-21 15:31:34 -0400

    completed first set of slides.

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -21,5 +21,9 @@ class EpicenterController < ApplicationController
   end
 
   def unfollow
+    current_user.following.delete(params[:id].to_i)
+    current_user.save
+    redirect_to show_user_path(id: params[:id])   
   end
+
 end
